c9e5a1de289459f566bfba29b2b9b25e6f12b3c5
nguyenthu2667 <hoangpro22@gmail.com>
2023-05-30 22:29:10 +0700

    update design Database and manhinhchinh

---
+++ QuanLyPhongTro/TaiKhoanModel.swift
@@ -0,0 +1,17 @@
+//
+//  TaiKhoanModel.swift
+//  QuanLyPhongTro
+//
+//. Nhóm 11
+//
+
+import UIKit
+
+class TaiKhoanModel: NSObject {
+    
+    var id: Int?
+    var tennguoidung: String?
+    var tendangnhap: String?
+    var matkhau: String?
+    
+}
