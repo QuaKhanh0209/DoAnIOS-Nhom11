5ea74a1dec4bdacc0c1d19cb09a7532f53448a85
nguyenthu2667 <hoangpro22@gmail.com>
2023-06-01 01:04:53 +0700

    Commit update

---
+++ QuanLyPhongTro/PhongModel.swift
@@ -0,0 +1,21 @@
+//
+//  PhongModel.swift
+//  QuanLyPhongTro
+//
+//. Nhóm 11
+//
+
+import UIKit
+
+class PhongModel: NSObject {
+    
+    var id: Int?
+    var tenphong: String?
+    var trangthai: Int?
+    var songuoi: Int?
+    var ngaydat: String?
+    var tienphong: Int?
+    var ngaythanhtoan: String?
+    var id_nguoitao: Int?
+    
+}
