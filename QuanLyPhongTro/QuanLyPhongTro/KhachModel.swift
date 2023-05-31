01f0595dfa791b641b1ef853c8893c75dac7ed61
nguyenthu2667 <hoangpro22@gmail.com>
2023-06-01 01:11:11 +0700

    git config nguyenthu

---
+++ QuanLyPhongTro/QuanLyPhongTro/KhachModel.swift
@@ -0,0 +1,19 @@
+//
+//  KhachModel.swift
+//  QuanLyPhongTro
+//
+//. Nhóm 11
+//
+
+import UIKit
+
+class KhachModel: NSObject {
+    
+    var id: Int?
+    var hoten: String?
+    var sdt: String?
+    var diachi: String?
+    var id_nguoitao: Int?
+    var id_phong: Int?
+    
+}
